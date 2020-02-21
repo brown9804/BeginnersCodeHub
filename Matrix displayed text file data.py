633eaea7d705e91513027c0c21cca6d6af8cc6c1
Belinda Brown <belindabrownr04@gmail.com>
2020-02-21 00:03:22 -0600

    Matriz a partir de un .txt
    La idea es tomar cualquier archivo .txt y separar los elementos mediante "," y de ahí generar la estructura de una matriz.

---
+++ Matrix displayed text file data.py
@@ -0,0 +1,44 @@
+#Se requiere utilizar pyhton3
+
+#importando paquetes
+import numpy as np
+import sys
+
+#Dado que las instrucciones solicitan imprimir la matriz en la linea de comandos utilizando el archivo facilitado.
+#se considera que no se conoce el numero de filas o columnas
+
+#Se requiere leer un archivo
+
+def leerArchivo(nombreArchivo, columnas, filas):
+    arreglo = []
+    try:
+        archivo = open(nombreArchivo,"r")
+        for linea in archivo:
+            linea_nueva =linea.split(",")
+            for i in range(len(filas)):
+                for j in range(len(columnas)):
+                    arreglo.append(linea_nueva)
+                    #print(arreglo[int(filas)][int(columnas)], end = " ")
+        archivo.close()
+
+    except(FileNotFoundError):
+        print("Archivo No encontrado")
+    except(ValueError):
+        print("Error en los tipos-valores de datos")
+    return arreglo
+
+
+
+#Dado que el archivo ya existe lo unico que se requiere es imprimir la matriz
+#para lo cual se debe crear una funcion
+
+arhivoAExtraer = input("Digite el nombre del archivo a leer junto con su extensión siendo esta (.txt)\n")
+filas = input("Digite la cantidad de elementos por oración en el archivo (.txt)\n")
+columnas = input("Digite el numero de oraciones que posee el (.txt)\n")
+
+matriz = leerArchivo(arhivoAExtraer,columnas,filas)
+print("Matriz solicitada orden NxM")
+matriz_con_corchetas = np.matrix(matriz)
+matriz_sin_corchetas = str(matriz_con_corchetas)[1:-1]
+print (matriz_sin_corchetas)
+print()
