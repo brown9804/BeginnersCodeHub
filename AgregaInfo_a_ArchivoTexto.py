9011950fd554c945da4da5a9f09181f3f0c31d31
Belinda Brown <belindabrownr04@gmail.com>
2020-03-28 14:47:40 -0600

    Agregando comentarios y explicaciones

--- AgregaInfo_a_ArchivoTexto.py
+++ AgregaInfo_a_ArchivoTexto.py
@@ -1,10 +1,16 @@
+#Este código permite observar como se puede agregar texto a un archivo ya existe.
+#Se ejecuta mediante:
+# python3 AgregaInfo_a_ArchivoTexto.py
+
+#comando para abrir el archivo donde se desea escribir 
 archivo = open("Himno.txt", "a") 
 
+#agregando líneas en el archivo 
 archivo.write("En la lucha tenaz, de fecunda labor,\n") 
 archivo.write("que enrojece del hombre la faz;\n")
 archivo.write("conquistaron tus hijos - labriegos sencillos - \n") 
 archivo.write("eterno prestigio, estima y honor, \n")
 
-
+#cerrando el archivo
 archivo.close()
 
