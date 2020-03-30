a0c0f50023dd2b30c7d4bbf6a947d8db3306c212
Belinda Brown <belindabrownr04@gmail.com>
2020-03-30 16:33:32 -0600

    Update and rename temperature.py to ConversionTemperatura.py

--- ConversionTemperatura.py
+++ ConversionTemperatura.py
@@ -1,10 +1,18 @@
+#Python3 
+
+#### 			DEFINICIONES 			####
+
+#Definicion para convertir de grado Fahrenheit a Celsius
 def celsius(f):
 	cgrade = (f-32)*(5/9)
 	return cgrade
 
+#Definicion para convertir de grado Celsius a Fahrenheit 
 def fahren(c):
 	fgrade = ((9/5)*c) +32
 	return fgrade
+
+#### 			IMPLEMENTACION 			####
 
 fahgrado = float(input("Digite un valor en grados fahrenheit para convertirlo a celsius "))
 c = celsius(fahgrado)
