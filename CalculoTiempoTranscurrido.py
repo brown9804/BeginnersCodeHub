9997dc9af62ef551bbb82ab848f7dd6045f5bfa3
Belinda Brown <belindabrownr04@gmail.com>
2020-03-30 16:29:04 -0600

    Actualizando comentarios

--- CalculoTiempoTranscurrido.py
+++ CalculoTiempoTranscurrido.py
@@ -1,14 +1,21 @@
+#Python3 
+
+
 #Se solicita datos
-comp = input("Digite un año para averiguar cuánto timpo ha transcurrido o cuánto falta para llegar ")
-#Procedimeinto si es un año futuro
+comp = input("Digite un año para averiguar cuánto tiempo ha transcurrido o cuánto falta para llegar ")
+
+#Si la fecha está dentro del 2018
 if (int(comp) == 2018):
 	print("Estamos en el año que digitó, no falta tiempo para llegar al 2018 ")
+
+#Procedimiento si es un año futuro, es decir, posterior al 2018
 elif(int(comp) >= 2018):
 	print("El año digitado es mayor al año en que estamos")
 	falt = int(comp) - 2018
 	print ("Faltan " +str(falt) +" años")
 	if (falt == 1):
 		print("Falta un año")
+
 #si ya pasó el año
 else:
 	print("Vamos a calcular cuántos años han pasado ")
