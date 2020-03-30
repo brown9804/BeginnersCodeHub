5a4c5235b8e95b89f13bc2beb617d16bd28858fe
Belinda Brown <belindabrownr04@gmail.com>
2020-03-30 16:30:45 -0600

    Update and rename terna_pitag.py to Terna_Pitagorica.py

--- Terna_Pitagorica.py
+++ Terna_Pitagorica.py
@@ -1,7 +1,9 @@
 print (" Este programa sirve para verificar si es una terna pitagórica")
+
 c1 = input ("Ingrese el valor de un valor ")
 c2 = input ("Por favor, digite el segundo dato: ")
 h = input("Digite e tercer dato: ")
+
 if (int(h)**2 == int(c1)**2 + int(c2)**2): #compara primeros dos datos con el tercero
 	print ("Los números " + c1, c2, h +" sí son una terna") #imprime solo si sí se cumple
 elif  (int(c1)**2 == int(h)**2 +  int(c2)**2): # compara primer dato con los otros dos 
