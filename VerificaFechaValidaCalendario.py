dd72ba71ac977591758aafc4e9fb6b0e2fc1c719
Belinda Brown <belindabrownr04@gmail.com>
2020-03-30 19:15:06 -0600

    Comentando

--- VerificaFechaValidaCalendario.py
+++ VerificaFechaValidaCalendario.py
@@ -1,23 +1,34 @@
+#Python3 
+
+#Permite Verificar la fecha valida ingresada respecto a la forma en la que se desarrolló el calendario lunar.
+
+###### 					DEFINICIONES 					######
+
+#Veriica si el dia es valido, es decir 1-31 dias máximo tiene un mes
 def dia_v(d):
 	if (d<1) or (d>31):
 		return False
 	else:
 		return True
+#Verifica si el numero de mes es valido, un año posee doce meses
 def mes_v(m):
 	if (m<1) or (m>12):
 		return False 
 	else:
 		return True
+#Funcion que permite mas verificar si el año es valido, contemple que el else posee el true
 def year_v(a):
 	if (a<1582):
 		return False
 	else:
 		return True
+#Averigua si el año es bisiesto dadas algunas condiciones teoricas 
 def bisiesto(year):
 	if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
 		return True
 	else:
 		return False
+#Definicion que abarca las anteriores para verificar si la fecha es valida
 def fecha_valida(dia, mes, an):
 	if dia_v(dia) and mes_v(mes) and year_v(an):
 
@@ -33,6 +44,8 @@ def fecha_valida(dia, mes, an):
 			print ("Época digitada es válida ")
 	else: 
 		print ("La fecha digitada es inválida")
+
+###### 					IMPLEMENTACION 					######
 
 day = int(input("Digite el día "))
 month = int(input("DIgite el mes "))
