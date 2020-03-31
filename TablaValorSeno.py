ebee0ca07341c96fa4cfc11d4f528d7a4e545548
Belinda Brown <belindabrownr04@gmail.com>
2020-03-30 18:23:28 -0600

    Comentando

--- TablaValorSeno.py
+++ TablaValorSeno.py
@@ -1,5 +1,10 @@
+###### 				IMPORTANDO PAQUETES 				######
 from math import pi
 from math import sin 
+
+###### 				DEFINICIONES 				######
+
+#Defincion de la funcion seno
 def sinc(x):
 	if x !=0 :
 		result = (sin(pi*x))/(pi*x)	
@@ -7,10 +12,12 @@ def sinc(x):
 		result = 1
 	return result 
 
+#Creando una definicion para modelar el incremento
 def incre(a,b):
 	inc = (b-a)/10
 	return inc
 
+#Funcion que muestra tabla con el valor y su seno calculado
 def tabla(a, b):
 	print("x                sinc(x)")
 	for indice in range (0,11):
@@ -18,6 +25,9 @@ def tabla(a, b):
 		variable = a + (indice*incremento)
 		seno = sinc(variable)
 		print("{0:5.2f}           {1:4.20f}       ".format(variable, seno))
+
+###### 				IMPLEMENTANDO 				######
+
 limsup = int(input("Digite el límite superior en el que desea evaluar la función "))
 liminf = int(input("Ingrese el límite inferior en el que quiere evaluar la función ")) 	
 
