39f518cc53fb40addaed03bdd9af746a3f6ccd26
Belinda Brown <belindabrownr04@gmail.com>
2020-03-30 22:09:24 -0600

    Comentando

--- AgregarLineasTXT.py
+++ AgregarLineasTXT.py
@@ -1,21 +1,25 @@
-#Writting first part
+#Python3 
+
+# Permite ejemplificar como agregar lineas de texto a un archivo extension .txt
+
+#Agregando lineas a un archivo de texto
 archivo = open("Himno.txt", "w")
 
 archivo.write("¡Noble patria!, tu hermosa bandera\n") 
-archivo.write("expresión de tu vida nos da;\n") 
-archivo.write("bajo el límpido azul de tu cielo\n")
+archivo.write("expresión de tu vida nos da;\n") 
+archivo.write("bajo el límpido azul de tu cielo\n")
 archivo.write("blanca y pura descansa la paz.\n")
 
 archivo.close()
 
-#Giving permission for reading
+#Cediendo permisos para leer el archivo
 #archivo = open("Himno.txt", "r")
 
 #for linea in archivo: 
 	#print(linea)
 #archivo.close()
 
-#deleting \n
+#Eliminando los saltos de linea \n
 archivo = open("Himno.txt", "r")
 for linea in archivo:
 	hilera = linea.replace("\n","") 
