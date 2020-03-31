caaf945afec6e7ffd074a20759a546e8bc18452f
Belinda Brown <belindabrownr04@gmail.com>
2020-03-30 20:58:00 -0600

    Comentado

--- num_primo.py
+++ num_primo.py
@@ -1,9 +1,19 @@
+#Python3
+
+### Permite verificar si un numero ingresado es primo o no 
+
+######				DEFINICIONES				######
+
 def primo_o_no(num, fac):
 	if (num % fac == 0):
 		return True
 	else:
 		return False
+
+######				IMPLEMENTANDO				######
+
 numero = int(input("Digite un número para verificar si es primo o no "))
+#Contador antes del ciclo
 cont = 0
 for factor in range (2, numero):
 	if primo_o_no(numero, factor) == True:
