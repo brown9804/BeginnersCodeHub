4fd4650698d6078b9592fc24e4bba1b1a035925c
Belinda Brown <belindabrownr04@gmail.com>
2020-03-30 21:06:44 -0600

    Comentado

--- CreaFiguraConSimbolos.py
+++ CreaFiguraConSimbolos.py
@@ -1,12 +1,21 @@
+#Python3
+
+#Crea una figura con un simbolo digitado
+
+######				DEFINICIONES				###### 
+
+
 def impr (anch):
 	print("*" * anch)
-ancho = int(input("Digite el ancho para el de asteriscos "))
-for indice in range (1, ancho + 1):
-	impr(ancho)
-
 
 def anchofig(anc,sym):
 	print (sym*anc)
+	
+######				IMPLEMENTACION				###### 
+
+ancho = int(input("Digite el ancho para el de asteriscos "))
+for indice in range (1, ancho + 1):
+	impr(ancho)
 ancho = int(input("Digite el ancho que desea para la figura "))
 alto = int(input("Digite el alto que desea para la figura "))
 symbol = input("Digite el símbolo con el que desea contruir la figura ")
